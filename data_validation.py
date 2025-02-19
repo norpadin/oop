e2faf44d17e4e0b32a58df1f168420e7b2470d28
Norberto Padin <npadin@gmail.com>
2025-02-19 14:57:33 -0300

    updated file

--- data_validation.py
+++ data_validation.py
@@ -1,30 +1,31 @@
 from datetime import datetime
 
 from pydantic import BaseModel, PositiveInt
+import pprint
 
 
 class User(BaseModel):
-    id: int  
-    name: str = 'John Doe'  
-    signup_ts: datetime | None  
-    tastes: dict[str, PositiveInt]  
+    id: int
+    name: str = 'John Doe'
+    signup_ts: datetime | None
+    tastes: dict[str, PositiveInt]
 
 
 external_data = {
     'id': 123,
-    'signup_ts': '2019-06-01 12:22',  
+    'signup_ts': '2019-06-01 12:22',
     'tastes': {
         'wine': 9,
-        b'cheese': 7,  
-        'cabbage': '1',  
+        b'cheese': 7,
+        'cabbage': '1',
     },
 }
 
-user = User(**external_data)  
+user = User(**external_data)
 
-print(user.id)  
-#> 123
-print(user.model_dump())  
+print(user.id)
+# > 123
+pprint.pprint(user.model_dump())
 """
 {
     'id': 123,
